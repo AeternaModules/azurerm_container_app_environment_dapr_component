cfc2ec33b42ee8a7bc401f89f484834e37ff03b7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:37:24 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "container_app_environment_dapr_components" {
+  description = <<EOT
+Map of container_app_environment_dapr_components, attributes below
+Required:
+    - component_type
+    - container_app_environment_id
+    - name
+    - version
+Optional:
+    - ignore_errors
+    - init_timeout
+    - scopes
+    - metadata (block):
+        - name (required)
+        - secret_name (optional)
+        - value (optional)
+    - secret (block):
+        - identity (optional)
+        - key_vault_secret_id (optional)
+        - name (required)
+        - value (optional)
+EOT
+
+  type = map(object({
+    component_type               = string
+    container_app_environment_id = string
+    name                         = string
+    version                      = string
+    ignore_errors                = optional(bool, false)
+    init_timeout                 = optional(string, "5s")
+    scopes                       = optional(list(string))
+    metadata = optional(object({
+      name        = string
+      secret_name = optional(string)
+      value       = optional(string)
+    }))
+    secret = optional(object({
+      identity            = optional(string)
+      key_vault_secret_id = optional(string)
+      name                = string
+      value               = optional(string)
+    }))
+  }))
+}
+
