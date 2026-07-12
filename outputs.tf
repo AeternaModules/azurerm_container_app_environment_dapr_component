a296e134166b9d624955135b144460a311bcbd21
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,38 @@
-output "container_app_environment_dapr_components" {
-  description = "All container_app_environment_dapr_component resources"
-  value       = azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components
-  sensitive   = true
-}
 output "container_app_environment_dapr_components_component_type" {
-  description = "List of component_type values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.component_type]
+  description = "Map of component_type values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.component_type }
 }
 output "container_app_environment_dapr_components_container_app_environment_id" {
-  description = "List of container_app_environment_id values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.container_app_environment_id]
+  description = "Map of container_app_environment_id values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.container_app_environment_id }
 }
 output "container_app_environment_dapr_components_ignore_errors" {
-  description = "List of ignore_errors values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.ignore_errors]
+  description = "Map of ignore_errors values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.ignore_errors }
 }
 output "container_app_environment_dapr_components_init_timeout" {
-  description = "List of init_timeout values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.init_timeout]
+  description = "Map of init_timeout values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.init_timeout }
 }
 output "container_app_environment_dapr_components_metadata" {
-  description = "List of metadata values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.metadata]
+  description = "Map of metadata values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.metadata }
 }
 output "container_app_environment_dapr_components_name" {
-  description = "List of name values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.name]
+  description = "Map of name values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.name }
 }
 output "container_app_environment_dapr_components_scopes" {
-  description = "List of scopes values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.scopes]
+  description = "Map of scopes values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.scopes }
 }
 output "container_app_environment_dapr_components_secret" {
-  description = "List of secret values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.secret]
+  description = "Map of secret values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.secret }
   sensitive   = true
 }
 output "container_app_environment_dapr_components_version" {
-  description = "List of version values across all container_app_environment_dapr_components"
-  value       = [for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : v.version]
+  description = "Map of version values across all container_app_environment_dapr_components, keyed the same as var.container_app_environment_dapr_components"
+  value       = { for k, v in azurerm_container_app_environment_dapr_component.container_app_environment_dapr_components : k => v.version }
 }
 
